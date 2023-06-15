828083e2396c9f638012d04ad323addf6df9ca65
Laura <laura.ogbechie@outlook.com>
2023-06-15 08:06:19 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point
+ *
+ * Return: This returns 0 if successful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
