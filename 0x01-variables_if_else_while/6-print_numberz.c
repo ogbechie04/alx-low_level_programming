96fad5de8cc92d31e1ba32cf91f8cdce1be23f33
Laura <laura.ogbechie@outlook.com>
2023-06-18 02:52:03 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point
+ *
+ * Return: This returns 0 if successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
