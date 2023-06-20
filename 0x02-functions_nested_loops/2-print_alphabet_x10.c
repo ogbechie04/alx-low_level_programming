e7892ec7fb0e98fd209cb07ec71f39e10d6a407d
Laura <laura.ogbechie@outlook.com>
2023-06-20 15:35:36 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - This prints the alphabets 10times
+ */
+
+void print_alphabet_x10(void);
+{
+	int i;
+	char j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		j = 97;
+
+		while (j <= 122)
+		{
+			_putchar(j);
+			j++;
+		}
+	}
+}
