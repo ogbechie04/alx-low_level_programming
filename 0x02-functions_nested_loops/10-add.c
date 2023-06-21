42fb43ded2abe152966ff1f4f1ce1efbaa0acfc5
Laura <laura.ogbechie@outlook.com>
2023-06-21 10:44:21 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @i: integer one
+ * @j: integer two
+ *
+ * Return: returns the result of the addition
+ */
+
+int add(int i, int j)
+{
+	int result;
+
+	result = (i + j);
+
+	return(result);
+}
