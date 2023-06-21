52872c30cd2028fc9a85a698f83ce3b92959c54a
Laura <laura.ogbechie@outlook.com>
2023-06-21 15:29:34 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+
+int main()
+{
+	print_to_98(-50);
+
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints numbers from n to 98
+ *
+ * @n: starting number to be inputted
+ *
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else if (n == 98)
+	{
+		printf("%d", n);
+	}
+
+	printf("\n");
+}
