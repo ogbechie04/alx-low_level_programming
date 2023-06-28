c3dfa21c39c6fe12c31c8d9280e77db0736c409e
Laura <laura.ogbechie@outlook.com>
2023-06-27 17:35:30 -0700

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - copies the string
+ *
+ * @dest: Destination
+ * @src: Source
+ *
+ * Return: the pointer to dest
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
