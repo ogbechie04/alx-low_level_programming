cf987a5779f54f59cfdd180185877d92ca907539
Laura <laura.ogbechie@outlook.com>
2023-07-03 12:19:41 -0700

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: string
+ * @c: character to locate
+ *
+ * Return: c if in the string and NULL if not
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+
+	return (NULL);
+}
