283884990f3b14297369cf3c31ec439c222a1160
Laura <laura.ogbechie@outlook.com>
2023-07-09 06:47:00 -0700

    a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ * @argv: argumernt vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
