c2b7e1e6690418ac5be64ad79823f8d78a5b034c
Laura <laura.ogbechie@outlook.com>
2023-07-09 06:29:42 -0700

    main.h

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc, int *argv[]);
+
+
+
+#endif /* MAIN_H */
