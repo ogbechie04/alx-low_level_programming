52f9c12dd37273b2a71079f9fa5a468c6f64b0fa
Laura Ogbechie <laura.ogbechie@outlook.com>
2023-07-18 00:30:03 +0100

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_FILE
+#define DOG_FILE
+
+/**
+ * struct dog - structure dog
+ * @name: atribute pointer to char name's dog
+ * @age: atribute float age's dog
+ * @owner: atribute pointer to char owner's dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_FILE */
