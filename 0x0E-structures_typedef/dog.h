0a52610ea7656d2d74ea9711c33305df0fdc5dda
Laura Ogbechie <laura.ogbechie@outlook.com>
2023-07-18 16:41:43 +0100

    Define a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,6 @@
-#ifndef DOG_FILE
-#define DOG_FILE
+#ifndef POPPY
 
-/**
- * dog_t - typedef for struct dog
- */
-typedef struct dog dog_t;
+#define POPPY
 
 /**
  * struct dog - structure dog
@@ -20,9 +16,4 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif
+#endif /* POPPY */
