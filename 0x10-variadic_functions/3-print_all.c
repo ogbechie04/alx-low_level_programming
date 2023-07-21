fc794a5a50682b6c40180b62e7ffdde4fe127b00
Laura Ogbechie <laura.ogbechie@outlook.com>
2023-07-21 01:10:44 +0100

    a function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,44 +11,44 @@
 
 void print_all(const char * const format, ...)
 {
-    int i = 0;
-    char *ptf, *seperator = "";
+	int i = 0;
+	char *ptf, *seperator = "";
 
-    va_list(p_all);
-    va_start(p_all, format);
+	va_list(p_all);
+	va_start(p_all, format);
 
-    if (format)
-    {
-        while (format[i])
-        {
-            switch (format[i])
-            {
-            case 'c':
-                printf("%s%c", seperator, va_arg(p_all, int));
-                break;
-            case 'i':
-                printf("%s%d", seperator, va_arg(p_all, int));
-                break;
-            case 'f':
-                printf("%s%f", seperator, va_arg(p_all, double));
-                break;
-            case 's':
-                ptf = va_arg(p_all, char *);
-                if (ptf == NULL)
-                {
-                    ptf = "(nil)";
-                }
-                printf("%s%s", seperator, ptf);
-                break;
-            default:
-                i++;
-                continue;
-            }
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", seperator, va_arg(p_all, int));
+					break;
+				case 'i':
+					printf("%s%d", seperator, va_arg(p_all, int));
+					break;
+				case 'f':
+					printf("%s%f", seperator, va_arg(p_all, double));
+					break;
+				case 's':
+					ptf = va_arg(p_all, char *);
+					if (ptf == NULL)
+					{
+						ptf = "(nil)";
+					}
+					printf("%s%s", seperator, ptf);
+					break;
+				default:
+					i++;
+					continue;
+			}
 
-            seperator = ", ";
-            i++;
-        }
-    }
-    printf("\n");
-    va_end(p_all);
+			seperator = ", ";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(p_all);
 }
